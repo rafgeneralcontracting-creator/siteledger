8da23ce2d70ea59346bb1de7d32c11ff671e18f4
rafgeneralcontracting-creator <rafgeneralcontracting@gmail.com>
2026-08-28 16:12:43 -0400

    Refresh PWA cache for drawing annotations

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='siteledger-v17';
-const SHELL=['./','./index.html','./styles.css?v=12','./drawings.css?v=4','./drawing-workspace.css?v=1','./app.js?v=12','./drawings.js?v=4','./drawing-workspace.js?v=1','./manifest.webmanifest','./icon.svg'];
+const CACHE='siteledger-v18';
+const SHELL=['./','./index.html','./styles.css?v=12','./drawings.css?v=4','./drawing-workspace.css?v=1','./drawing-annotations.css?v=1','./app.js?v=12','./drawings.js?v=4','./drawing-workspace.js?v=1','./drawing-annotations.js?v=1','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting()});
